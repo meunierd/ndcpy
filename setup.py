51703e52ee59ddfa5b01278bce15ca88192fa3be
Devon Meunier <devon.meunier@shopify.com>
2017-11-16 13:22:50 -0500

    v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ndcpy',
-    version='0.1.1',
+    version='0.2.0',
     description='A Python wrapper for NDC',
     url='https://github.com/meunierd/ndcpy',
     author='Devon Meunier',
